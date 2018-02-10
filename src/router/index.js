1fd7425b290f247218d2896c4d33be7b71df378b
Maius <bow810531@gmail.com>
2018-02-10 18:41:27 +0800

    add router

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+import Home from '@/components/Home';
+import Like from '@/components/Like';
 import HelloWorld from '@/components/HelloWorld';
 
 Vue.use(Router);
@@ -8,7 +10,17 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
+      name: '/',
+      component: Home,
+    },
+    {
+      path: '/',
+      name: 'Like',
+      component: Like,
+    },
+    {
+      path: '/',
+      name: 'manage',
       component: HelloWorld,
     },
   ],
